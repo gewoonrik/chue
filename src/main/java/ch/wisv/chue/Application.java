8a2dc10532078a853927465a2b4add83191a9088
Mark Janssen <mark@praseodym.net>
2015-02-25 14:47:13 +0100

    Cleanup Spring MVC initializer

--- src/main/java/ch/wisv/chue/Application.java
+++ src/main/java/ch/wisv/chue/Application.java
@@ -3,12 +3,8 @@ package ch.wisv.chue;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.*;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
 
-@EnableAutoConfiguration
-@Configuration
-@ComponentScan
+@SpringBootApplication
 public class Application {
 
     public static void main(String[] args) throws Exception {
