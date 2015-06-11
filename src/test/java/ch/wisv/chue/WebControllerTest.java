7b80c5c13c474fb1dd7c457ab24b269732c7c825
Rick Wieman <git@rickw.nl>
2015-06-11 22:29:10 +0200

    Switches tests to MockMvc

--- src/test/java/ch/wisv/chue/WebControllerTest.java
+++ src/test/java/ch/wisv/chue/WebControllerTest.java
@@ -5,9 +5,13 @@ import org.junit.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
 import static org.hamcrest.CoreMatchers.is;
-import static org.hamcrest.MatcherAssert.assertThat;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 public class WebControllerTest {
     @Mock
@@ -16,36 +20,40 @@ public class WebControllerTest {
     @InjectMocks
     private WebController webController;
 
+    private MockMvc mockMvc;
+
     @Before
     public void setUp() {
         MockitoAnnotations.initMocks(this);
+
+        mockMvc = MockMvcBuilders.standaloneSetup(webController).build();
     }
 
     @Test
-    public void testColorHexAll() {
-        String response = webController.color("all", "ff0000");
-
-        assertThat(response, is("Changed colour of lamps (all) to #ff0000"));
+    public void testColorHexAll() throws Exception {
+        mockMvc.perform(get("/color/all/ff0000"))
+                .andExpect(status().isOk())
+                .andExpect(content().string(is("Changed colour of lamps (all) to #ff0000")));
     }
 
     @Test
-    public void testColorHexID() {
-        String response = webController.color("1", "0000ff");
-
-        assertThat(response, is("Changed colour of lamps (1) to #0000ff"));
+    public void testColorHexID() throws Exception {
+        mockMvc.perform(get("/color/1/0000ff"))
+                .andExpect(status().isOk())
+                .andExpect(content().string(is("Changed colour of lamps (1) to #0000ff")));
     }
 
     @Test
-    public void testColorFriendlyAll() {
-        String response = webController.colorFriendly("all", "red");
-
-        assertThat(response, is("Changed colour of lamps (all) to #ff0000"));
+    public void testColorFriendlyAll() throws Exception {
+        mockMvc.perform(get("/color/all/red"))
+                .andExpect(status().isOk())
+                .andExpect(content().string(is("Changed colour of lamps (all) to #ff0000")));
     }
 
     @Test
-    public void testColorFriendlyID() {
-        String response = webController.colorFriendly("2", "blue");
-
-        assertThat(response, is("Changed colour of lamps (2) to #0000ff"));
+    public void testColorFriendlyID() throws Exception {
+        mockMvc.perform(get("/color/2/blue"))
+                .andExpect(status().isOk())
+                .andExpect(content().string(is("Changed colour of lamps (2) to #0000ff")));
     }
 }
